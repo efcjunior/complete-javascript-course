abff0f54157e166815b3de56515a9cb9bdc08c2a
Everson Junior <everson.c.junior@gmail.com>
2020-05-29 00:32:08 -0300

    lessons: objects and functions

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -68,3 +68,123 @@ var youForm = Object.create(contactFormProto,
  * 
  * function constructor - the newly created object inherits from the constructor´s prototype property.
  */
+
+
+ /**Primitive vs Objects */
+
+ /**
+  * Primitive: copy value
+  * integer
+  * string
+  * boolean
+  * null
+  * undefined
+  * 
+  * Object: everthing else
+  */
+  var a = 23;
+  var b = a;
+
+  a = 46;
+
+  console.log(a);
+  console.log(b);
+
+  var obj1 = {
+      name: 'John',
+      age: 26
+  }
+
+  //objects
+var obj2 = obj1;
+obj1.age = 30;
+console.log(obj1.age);
+console.log(obj2.age);
+
+//functions
+
+var age = 27;
+var obj3 = {
+    name: 'Jonas',
+    city: 'Lisbon'
+}
+
+function change(a,b){
+    a = 30;
+    b.city = 'San Francisco';
+}
+
+change(age, obj3);
+
+console.log(age);
+console.log(obj3.city);
+
+/**
+ * First Class Functions: Passing functions as arguments
+ */
+
+ /**
+  * 1) functions area also objects in Javascript 
+  * - they are instance of the object type.
+  * - behave like any other object
+  * - can be stored in a variable
+  * - can be passed as an argument to another function
+  * - can be returned from a function
+  */
+
+  var years = [1990,1965,1937,2005,1998];
+
+  function arrayCalc(arr, fn) {
+      var arrRes = [];
+      for(var i = 0; i < arr.length; i++) {
+          arrRes.push(fn(arr[i]));
+      }
+      return arrRes;
+  }
+
+  //callback function 1
+  function newCalculateAge(el) {
+       return 2020 - el;
+  }
+
+  console.log(arrayCalc(years, newCalculateAge));
+
+  function isFullAge(el){
+      return el >= 18;
+  }
+
+  console.log(arrayCalc(years, isFullAge));
+
+  function maxHeartRate(el) {
+        if(el >= 18 && el <= 81){
+            return Math.round(206.9 - (0.67 * el));
+        } else {
+            return -1;
+        }
+  }
+
+  console.log(arrayCalc(arrayCalc(years, newCalculateAge), maxHeartRate));
+
+/**
+ * First Class Functions: Functions Returning Functions
+ */
+
+ function interviewQuestion(job) {
+     if (job === 'designer') {
+         return function (name) {
+             console.log(name + ', can you explain what UX design is ? ');
+         }
+     } else if (job === 'teacher') {
+         //anonymous function
+         return function (name) {
+             console.log('What subject do you teach, ' + name + '?');
+         }
+     } else {
+         return function (name) {
+             console.log('Hello ' + name + ', what do you do ? ');
+         }
+     }
+ }
+
+ interviewQuestion('teacher')('Everson');
+ interviewQuestion('designer')('Rute');
