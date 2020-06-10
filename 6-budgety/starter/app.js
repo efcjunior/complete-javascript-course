108d2f6d84a0ce28686cd4980c83faf565f6f17d
Everson Junior <everson.c.junior@gmail.com>
2020-06-10 00:01:35 -0300

    add starter lesson code

---
+++ 6-budgety/starter/app.js
@@ -0,0 +1,35 @@
+/*Module pattern implementation structure */
+
+var controllerXyz01 = (function(){
+
+    //data encapsulation
+
+
+    return {
+        //interface
+    }
+
+})();
+
+
+var controllerXyzn = (function(){
+    //data encapsulation
+
+
+    return {
+        //interface
+    }
+
+})();
+
+
+//Global app controller 
+var controller = (function(ctrl01, ctrln){
+
+    //event listerners
+
+    return {
+
+    }
+
+})(controllerXyz01,controllerXyzn);
