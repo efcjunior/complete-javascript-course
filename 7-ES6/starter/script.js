e4974f0a85b10e8f2c7173d17d7544315108fefd
efcjunior <everson.c.junior@gmail.com>
2020-06-27 20:02:46 -0300

    add section 7 until arrow function lexical this keyword

---
+++ 7-ES6/starter/script.js
@@ -0,0 +1,253 @@
+// Lecture: let and const
+
+// ES5
+var name5 = 'Laby Junior';
+var age5 = 35;
+names5 = 'Rodrigo Couch';
+
+// ES6
+const name6 = 'Laby Junior';
+let age6 = 35;
+//name6 = 'Rodrigo Couch'; Uncaught typeError: assignment to constant variable
+
+// ES5 vs ES6
+
+function driversLicenseES5(passedTest) {
+    if (passedTest){
+        //console.log(firstName); output: undefined - variable hoisted and acessible
+        var firstName = 'Laby';
+        var yearOfBirth = '1990';
+        //console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a car');
+    }
+    console.log(firstName + ', born in ' + yearOfBirth 
+    + ', is now officially allowed to drive a car'); //Laby, born in 1990, is now officially allowed to drive a car
+}
+
+driversLicenseES5(true);
+
+
+function driversLicenseES6(passedTest) {
+    if (passedTest){
+        //console.log(firstName); Uncaught ReferenceError: Cannot access 'firstName' before initialization variable hoisted and inacessible
+        let firstName = 'Laby';
+        const yearOfBirth = '1990';
+        console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a car');
+    }
+    /*console.log(firstName + ', born in ' + yearOfBirth 
+    + ', is now officially allowed to drive a car'); Uncaught ReferenceError: firstName is not defined*/
+}
+
+driversLicenseES6(true);
+
+/*
+NOTE 1: LET AND CONST declaration use block scope ({---scope---}), while VAR declaration uses function scope.
+*/
+
+
+let i = 23; //block 1
+
+for (let i = 0; i < 5; i++) {
+    //block 2
+    console.log(i);
+}
+
+console.log(i);
+
+/*
+NOTE 2: The blocks don´t change each other.
+*/
+
+
+var j = 50; //block 1
+
+for (var j = 0; j < 5; j++) {
+    //block 2
+    console.log(j);
+}
+
+console.log(j);
+
+/*
+NOTE 3: The blocks change
+*/
+
+//----------------------------------------------------------------------------------------------------------------------------------
+
+// Lecture: Blocks IIFEs
+
+// ES6
+{
+    //block scope
+    const a = 1;
+    let b = 2;
+    var c = 3 
+}
+
+//console.log(a + b);  Uncaught ReferenceError: a is not defined
+//console.log(c); output: 3 - This because var declaration is not block scope.
+
+// ES5
+(function () {
+    //function scope
+    var d = 5;
+});
+
+//console.log(d);  Uncaught ReferenceError: a is not defined.
+
+//----------------------------------------------------------------------------------------------------------------------------------
+
+// Lecture: Strings
+
+let firstName = 'John';
+let lastName = 'Smith';
+const yearOfBirth = 1990;
+
+function calcAge(year) {
+    return 2020 - year;
+};
+
+// ES5
+console.log('This is ' + firstName + ' ' + lastName + '. He was born in ' 
++ yearOfBirth + '. Today, he is ' + calcAge(yearOfBirth) + ' years old.');
+
+// ES6 - template literals `` (backticks)
+console.log(`This is ${firstName} ${lastName}. He was born in ${yearOfBirth}. Today, he is ${calcAge(yearOfBirth)} years old.`);
+
+// ES6 - util methods
+const n = `${firstName} ${lastName}`;
+console.log(n.startsWith('J'));
+console.log(n.endsWith('th'));
+console.log(n.includes('oh'));
+console.log(`${firstName} `.repeat(5));
+
+//----------------------------------------------------------------------------------------------------------------------------------
+
+// Lecture: Arrow functions
+
+const years = [1990, 1965, 1982, 1937];
+
+// ES5
+var ages5 = years.map(function(el){
+    return 2020 - el;
+});
+
+console.log(ages5);
+
+// ES6
+let ages6 = years.map(el => 2020 - el);
+console.log(ages6);
+
+ages6 = years.map((el, index) => `${index + 1}: ${2020 - el}.`);
+console.log(ages6);
+
+ages6 = years.map((el, index) => {
+    const now = new Date().getFullYear();
+    const age = now - el;
+    return `${index + 1}: ${age}.`    
+});
+console.log(ages6);
+
+//----------------------------------------------------------------------------------------------------------------------------------
+
+// Lecture: Arrow functions Lexical 'this' keyword
+
+// ES5
+var box5 = {
+    color: 'green',
+    position: 1,
+    clickMe: function() {
+        var self = this;
+
+        function display() {            
+            var str = 'This is box number ' + self.position 
+            + ' and it is ' + self.color;
+            alert(str);
+        }
+
+        document.querySelector('.green').addEventListener('click', display);
+    }
+}
+
+box5.clickMe();
+
+// ES6
+const box6 = {
+    color: 'green',
+    position: 1,
+    clickMe: function() {
+        
+        function display(self) {            
+            let str = `This is box number ${self.position} and it is  ${self.color}`;
+            alert(str);
+        }
+
+        document.querySelector('.green').addEventListener('click', () => display(this));
+    }
+}
+
+box6.clickMe();
+
+// ES6
+const box66 = {
+    color: 'green',
+    position: 1,
+    clickMe: () => {
+        /* 
+            Note 1: it output undefined, that´s because this method also shares the lexical this keyword from its surroundings.
+            And the surrounding of this is the global context.
+        */
+        function display(self) {            
+            let str = `This is box number ${self.position} and it is  ${self.color}`;
+            alert(str);
+        }
+        document.querySelector('.green').addEventListener('click', () => display(this));
+    }
+}
+
+box66.clickMe();
+
+
+// ES5
+function Person5(name) {
+    this.name = name;
+}
+
+Person5.prototype.myFriends5 = function(friends) {
+
+    /*var arr = Array.prototype.map.call(friends, function(el) {
+        //it´s going to point to the global object
+        return this.name + ' is friends with ' + el;
+    }, this);*/
+
+    /*var arr = friends.map(function(el) {
+        //it´s going to point to the global object
+        return this.name + ' is friends with ' + el;
+    }, this);*/
+
+    var arr = friends.map(function(el) {
+        //it´s going to point to the global object
+        return this.name + ' is friends with ' + el;
+    }.bind(this));
+
+    console.log(arr);
+};
+
+var friends5 = ['Bob', 'Jane', 'Mark'];
+
+new Person5('John').myFriends5(friends5);
+
+
+// ES6
+function Person6(name) {
+    this.name = name;
+}
+
+Person6.prototype.myFriends6 = function(friends) {   
+    var arr = friends.map((el) => `${this.name} is friends with  ${el}`);
+
+    console.log(arr);
+};
+
+var friends6 = ['Bob6', 'Jane6', 'Mark6'];
+
+new Person6('John').myFriends6(friends6);
