4369b68d12c547115f0d0c369c015ceb4327ed79
efcjunior <everson.c.junior@gmail.com>
2020-07-08 10:58:43 -0300

    add forkfy search feature

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,5 +1,41 @@
-// Global app controller
-import num from './test';
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements, renderLoader, clearLoader} from './views/base';
 
-const x = 23;
-console.log(`I imported ${num} from another module called test.js! Variable x is ${x}`);
+/*Global state of the app 
+ - search object
+ - current recipe object
+ - shopping list object
+ - Liked recipes
+*/
+const state = {
+
+}
+
+const controlSearch = async () => {
+    //1 Get query from view
+    const query = searchView.getInput();
+    console.log(query);
+
+    if(query) {
+        //2) new search object and add to state;
+        state.search = new Search(query);
+
+        //3) Prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
+        renderLoader(elements.searchRes);
+
+        //4) Search for recipes
+        await state.search.getResults();
+
+        //5) render results on UI
+        clearLoader();
+        searchView.renderResults(state.search.result);
+    }
+}
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
